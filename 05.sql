4883dc27363c1a4eb4693c7337a3360e32f20218
Gustavo295 <gustavomteixeira.2006@gmail.com>
2025-03-14 10:24:06 -0300

    14-03-25 sem exercicios

--- 05.sql
+++ 05.sql
@@ -12,3 +12,34 @@ END;
 
 SELECT COUNT(sal) FROM emp;
 ROLLBACK;
+
+DECLARE 
+    v_media NUMBER(9,2) := 0;
+    v_conta NUMBER(5) := 0;
+BEGIN
+    SELECT 
+        AVG(sal) INTO v_media
+        FROM emp;
+    UPDATE emp 
+        SET sal = sal * 1.10
+    WHERE sal < v_media;
+    v_conta := SQL%ROWCOUNT;
+    DBMS_OUTPUT.PUT_LINE(v_conta || 'Linhas atualizadas');
+END;
+/
+
+SELECT COUNT(sal) FROM emp;
+ROLLBACK;
+
+DECLARE 
+    v_maior NUMBER (9,2) := 0;
+BEGIN
+    SELECT 
+        MAX(sal) INTO v_maior
+        FROM emp;
+    INSERT INTO emp(empno, sal)
+    VALUES (4000, v_maior + 1000);
+END;
+/
+
+SELECT * FROM emp WHERE empno = 4000;
